5eb88b5f79a88a10e69266207044f7007ed3024d
adriandil0 <152308151+adriandil0@users.noreply.github.com>
2023-12-29 17:40:02 -0500

    tres numeros postivos y enteros

---
+++ Ejerc_1/Ejerc_1.py
@@ -0,0 +1,10 @@
+contador=0
+sumatoria=0
+while contador < 3:
+    num=int(input("Ingrese un numero : "))
+    if num < 0:
+        print("El numero no puede ser negativo")
+    else:
+        sumatoria=num+sumatoria
+        contador+=1
+print(f"El resultado de la suma es: {sumatoria}")
